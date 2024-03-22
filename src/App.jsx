4f0c451d22e2622aea0980f652943aa8da8621b8
Volkan <v.ocakli@gmail.com>
2024-03-22 20:11:41 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="text-5xl">App</div>;
+    </>
+  );
+};
+
+export default App;
